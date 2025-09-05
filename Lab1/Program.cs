db16736ead4af3f73a2cb7237643f60e3b7df09a
Kamilya Ziangirova <ziangirovakamilya@gmail.com>
2025-09-05 10:18:40 +0300

    Add files via upload

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -5,7 +5,7 @@
         public static void Main()
         {
             White white = new White();
-            //Console.WriteLine(white.Task1(1.2));
+            Console.WriteLine(white.Task1(1.2));
             Console.WriteLine(white.Task2(2));
             Console.WriteLine(white.Task3(25, 189));
             //Console.WriteLine(white.Task4(-2.5, 1.89));
