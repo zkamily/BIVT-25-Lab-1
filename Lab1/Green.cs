616be711b175a67eadaedd269612f248db937471
Kamilya Ziangirova <ziangirovakamilya@gmail.com>
2025-09-12 14:38:43 +0300

    Add files via upload

--- Lab1/Green.cs
+++ Lab1/Green.cs
@@ -7,7 +7,15 @@
             bool answer = false;
 
             // code here
-
+            double m = Math.Abs(d);
+            if (m >= 1)
+            {
+                answer = true;
+            }
+            else
+            {
+                answer = false;
+            }
             // end
 
             return answer;
@@ -17,7 +25,11 @@
             bool answer = false;
 
             // code here
-
+            double sr = (d + f) / 2;
+            if (sr > 0)
+            {
+                answer = true;
+            }
             // end
 
             return answer;
@@ -27,7 +39,12 @@
             bool answer = false;
 
             // code here
-
+            int summ = a + b;
+            int ModSr = (Math.Abs(a) + Math.Abs(b)) / 2;
+            if (summ > ModSr)
+            {
+                answer = true;
+            }
             // end
 
             return answer;
@@ -37,7 +54,8 @@
             int answer = 0;
 
             // code here
-
+            int MaxAB = Math.Max(a, b);
+            answer = Math.Max(MaxAB, c);
             // end
 
             return answer;
@@ -47,7 +65,14 @@
             double answer = 0;
 
             // code here
-
+            if (x > 1 | x < -1)
+            {
+                answer = 0;
+            }
+            else
+            {
+                answer = x * x - 1;
+            }
             // end
 
             return answer;
@@ -57,7 +82,18 @@
             bool answer = false;
 
             // code here
-
+            if (y < 0)
+            {
+                answer = false;
+            }
+            else if ((x < 0) & (y <= (x + 1)))
+            {
+                answer = true;
+            }
+            else if ((x >= 0) & (y <= (1 - x)))
+            {
+                answer = true;
+            }
             // end
 
             return answer;
@@ -68,7 +104,17 @@
             bool answer = true;
 
             // code here
-
+            if (n >= 0)
+            {
+                if (n % 2 == 0)
+                {
+                    answer = false;
+                }
+            }
+            else
+            {
+                answer = false;
+            }
             // end
 
             return answer;
@@ -78,7 +124,17 @@
             bool answer = false;
 
             // code here
-
+            int SumY = 0;
+            if (X >= 7)
+            {
+                for (int i = 0; i < X; i++)
+                { 
+                    if ((i % 2) == 0) { SumY += Y; }
+                 
+                }
+                if ((SumY >= 4 * 60) && (SumY <= 6 * 60))
+                    answer = true;
+            }
             // end
 
             return answer;
